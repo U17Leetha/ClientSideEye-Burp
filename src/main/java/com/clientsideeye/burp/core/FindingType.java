6fa244500d64acfff09591fb7293abe2a527138e
Sweet_P <80220625+U17Leetha@users.noreply.github.com>
2026-01-23 15:49:54 -0500

    Add FindingType enum for various finding types

---
+++ src/main/java/com/clientsideeye/burp/core/FindingType.java
@@ -0,0 +1,8 @@
+package com.clientsideeye.burp.core;
+
+public enum FindingType {
+    PASSWORD_VALUE_IN_DOM,
+    HIDDEN_OR_DISABLED_CONTROL,
+    ROLE_PERMISSION_HINT,
+    INLINE_SCRIPT_SECRETISH
+}
